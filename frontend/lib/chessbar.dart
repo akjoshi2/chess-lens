7c320dfee643100df0a4f5ce35dc336c977f51c0
akjoshi2 <akjoshi2003@gmail.com>
2023-11-11 15:29:09 -0600

    Half black half white chessbar

--- frontend/lib/chessbar.dart
+++ frontend/lib/chessbar.dart
@@ -9,14 +9,30 @@ class ChessbarWidget extends StatefulWidget {
 class ChessbarState extends State<ChessbarWidget> {
   @override
   Widget build(BuildContext context) {
+    final List<Color> grad = [
+      Colors.white,
+      Colors.white,
+      Colors.black,
+      Colors.black
+    ];
+    double whitePercent = 50.00;
+    double whiteStop = (100 - whitePercent) / 100;
+    final List<double> stops = [0.0, whiteStop, whiteStop, 1.0];
+
     return Flexible(
       child: FractionallySizedBox(
-          widthFactor: 1,
+          widthFactor: .1,
           heightFactor: .5,
           child: Container(
-            decoration: const BoxDecoration(
-              color: Colors.black,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: grad,
+                stops: stops,
+                end: Alignment.bottomCenter,
+                begin: Alignment.topCenter,
+              ),
               shape: BoxShape.rectangle,
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
             ),
           )),
     );
