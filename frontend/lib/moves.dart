7a572be915286d49acbaa0302efa22b586a35528
akjoshi2 <akjoshi2003@gmail.com>
2023-11-12 00:50:38 -0600

    Move list

--- frontend/lib/moves.dart
+++ frontend/lib/moves.dart
@@ -7,19 +7,40 @@ class MovesWidget extends StatefulWidget {
 }
 
 class MovesState extends State<MovesWidget> {
-  final List<String> moves = ["Kxd8", "e5", "c5"];
+  final List<String> moves = [
+    "e4",
+    "e5",
+    "Nf3",
+    "Nc6",
+    "Bb5",
+    "Nf6",
+    "d3",
+    "Bc5",
+    "Ba4",
+    "O-O",
+    "O-O",
+    "Re8",
+    "c3",
+    "Bf8",
+    "Re1",
+    "h6",
+    "Nbd2",
+    "d6"
+  ];
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: Container(
-            height: 244,
-            padding: const EdgeInsets.all(10.0),
-            width: 78,
-            decoration: BoxDecoration(color: Colors.white),
-            child: ListView.builder(
-                itemCount: moves.length,
-                itemBuilder: (content, index) {
-                  return Text("${index + 1}. ${moves[index]}");
-                })));
+        child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+                height: 244,
+                padding: const EdgeInsets.all(10.0),
+                width: 78,
+                decoration: BoxDecoration(color: Colors.white),
+                child: ListView.builder(
+                    itemCount: moves.length,
+                    itemBuilder: (content, index) {
+                      return Text("${index + 1}. ${moves[index]}");
+                    }))));
   }
 }
